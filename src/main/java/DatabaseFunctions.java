cf9e08cbbdc03fb7b4ab23e81d40399579c96323
Alex <dexmount@hotmail.com>
2016-09-19 18:23:26 -0500

    Adding the CRUD methods. Will add test methods.

--- src/main/java/DatabaseFunctions.java
+++ src/main/java/DatabaseFunctions.java
@@ -1,9 +1,8 @@
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.SimpleAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
+import java.util.UUID;
 
 /**
  * Created by Dan on 9/8/2016.
@@ -142,8 +141,50 @@ public class DatabaseFunctions {
         }
     }
 
-    public static SimpleAccount RetrieveUser(String username)
-    {
-        return null;
+    public static void CreateUser(User newUser){
+        Execute("INSERT INTO users( userName, password, role, score) VALUES(?, ?, ?, ?);", new Object[]{newUser.getUserName(), newUser.getPassword(), newUser.getRole(), newUser.getScore()});
+
     }
+
+    public static User RetrieveUser(User user) {
+        try {
+            ResultSet rs = Retrieve("SELECT * FROM users WHERE userName='?'", new Object[]{user.getUserName()});
+            user.setPassword(rs.getString("password"));
+            user.setRole(rs.getString("role"));
+            user.setScore(rs.getInt("score"));
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static void UpdateUserScore(User user){
+        Execute("UPDATE users SET score = ? WHERE userName = '?';", new Object[]{user.getUserName(), user.getScore()});
+    }
+
+    public static void UpdateUserPassword(User user){
+        Execute("UPDATE users SET password = '?' WHERE userName = '?';", new Object[]{user.getPassword(), user.getUserName()});
+    }
+
+    public static void DeleteUser(User user){
+        Execute("DELETE FROM users WHERE userName='?'", new Object[]{user.getUserName()});
+    }
+
+    public static void CreateCoin(Coin newCoin){
+        Execute("INSERT INTO coins(uuid, initialUser) VALUES(?,?);", new Object[]{newCoin.getCoin(), newCoin.getInitialUser()});
+
+    }
+
+    public static String RetrieveCoinInitialUser(UUID id){
+        return Retrieve("SELECT initialUser FROM coins WHERE uuid='?'", new Object[]{id.toString()}).toString();
+    }
+
+    public static void DeleteCoin(UUID id){
+         Execute("DELETE FROM coins WHERE uuid='?';", new Object[]{id.toString()});
+
+    }
+
+
 }
