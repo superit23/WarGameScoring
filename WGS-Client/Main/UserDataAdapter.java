aeb81af9701728ed7a941739301d02e44024e1e2
Alex <dexmount@hotmail.com>
2016-11-22 16:36:29 -0600

    UserDataAdapter update

--- WGS-Client/Main/UserDataAdapter.java
+++ WGS-Client/Main/UserDataAdapter.java
@@ -1,8 +1,11 @@
 import bb.rackmesa.wargamescoring.IUserDataAdapter;
 import bb.rackmesa.wargamescoring.User;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
@@ -13,7 +16,8 @@ import java.io.IOException;
  */
 public class UserDataAdapter implements IUserDataAdapter {
     private String sessionCookie;
-    private String url = "http://127.0.0.1:8080/";
+    private String url = "http://127.0.0.1:8080/user";
+    private HttpClient client = HttpClientBuilder.create().build();
 
     public UserDataAdapter(){
 
@@ -22,10 +26,8 @@ public class UserDataAdapter implements IUserDataAdapter {
     @Override
     public User CreateUser(String username, String password, String role, String team, int score){
 
-        HttpResponse response = null;
         try {
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpPost post = new HttpPost(url + "user");
+            HttpPost post = new HttpPost(url);
             post.setHeader("cookie", sessionCookie);
             post.setHeader("username", username);
             post.setHeader("password", password);
@@ -33,32 +35,70 @@ public class UserDataAdapter implements IUserDataAdapter {
             post.setHeader("team", team);
             post.setHeader("score", Integer.toString(score));
 
-            response = client.execute(post);
+            HttpResponse response = client.execute(post);
+            System.out.println(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println(response);
+
         return null;
     }
 
     @Override
     public User RetrieveUser(String username) {
+        try {
+            HttpGet get = new HttpGet(url);
+            get.setHeader("cookie", sessionCookie);
+            get.setHeader("username", username);
+            HttpResponse response = client.execute(get);
+            System.out.println(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @Override
     public void UpdateUser(User user) {
+        try {
+            HttpPut put = new HttpPut(url);
+            put.setHeader("cookie", sessionCookie);
+            put.setHeader("username", user.getUserName());
+            put.setHeader("password", user.getPassword());
+            put.setHeader("role", user.getRole());
+            put.setHeader("team", user.getTeam());
+            put.setHeader("score", Integer.toString(user.getScore()));
+            HttpResponse response = client.execute(put);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void DeleteUser(User user) {
+        try {
+            HttpDelete delete = new HttpDelete(url);
+            delete.setHeader("cookie", sessionCookie);
+            delete.setHeader("username", user.getUserName());
+            HttpResponse response = client.execute(delete);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void DeleteUser(String username) {
-
+        try {
+            HttpDelete delete = new HttpDelete(url);
+            delete.setHeader("cookie", sessionCookie);
+            delete.setHeader("username", username);
+            HttpResponse response = client.execute(delete);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
